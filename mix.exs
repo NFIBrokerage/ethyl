754e925b992f4d8c28d4550dcae43dec2d8d9053
Michael Davis <mcarsondavis@gmail.com>
2021-09-23 14:39:32 -0500

    add testing deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Ethyl.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.html": :test,
+        "coveralls.github": :test,
+        docs: :dev,
+        bless: :test,
+        credo: :test
+      ]
     ]
   end
 
@@ -19,6 +28,10 @@ defmodule Ethyl.MixProject do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.24", only: [:dev], runtime: false},
+      {:credo, "~> 1.0", only: [:test], runtime: false},
+      {:excoveralls, "~> 0.14", only: [:test]},
+      {:bless, "~> 1.0", only: [:test]}
     ]
   end
 end
