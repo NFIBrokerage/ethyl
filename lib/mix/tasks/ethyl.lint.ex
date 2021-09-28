b0df2c969bbce9a94e70a82c5222f09aa79d381a
Michael Davis <mcarsondavis@gmail.com>
2021-09-28 11:24:05 -0500

    add a mix task for linting

---
+++ lib/mix/tasks/ethyl.lint.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Ethyl.Lint do
+  use Mix.Task
+
+  @moduledoc """
+  A mix task for running the Ethyl linters against specified files
+
+  The linter may be run against a single file like so
+
+      mix ethyl.lint path/to/ethyl/expression.exs
+
+  Or you may pass a glob which is expanded by your shell
+
+      mix ethyl.lint path/to/ethyl/*.exs
+  """
+
+  @shortdoc "Runs the ethyl linting suite against files"
+  def run(paths) do
+    paths
+    |> Task.async_stream(fn path ->
+      path
+      |> Ethyl.Source.new()
+      |> Ethyl.Lint.lint()
+    end)
+    |> Enum.flat_map(fn {:ok, result} -> result end)
+    |> IO.inspect(label: "lints")
+  end
+end
